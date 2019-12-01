11479632f5e2e774a98f1e42350ba4f4d56163d9
Christiaan Van den Poel <christiaan.vandenpoel@gmail.com>
2019-12-01 15:13:22 +0100

    initial commit

---
+++ lib/transport_tycoon_elixir.ex
@@ -0,0 +1,18 @@
+defmodule TransportTycoonElixir do
+  @moduledoc """
+  Documentation for TransportTycoonElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TransportTycoonElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/transport_tycoon_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule TransportTycoonElixirTest do
+  use ExUnit.Case
+  doctest TransportTycoonElixir
+
+  test "greets the world" do
+    assert TransportTycoonElixir.hello() == :world
+  end
+end
